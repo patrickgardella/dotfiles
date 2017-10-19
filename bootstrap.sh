41934a61530b876195b141766b0153e783ac5c3e
Patrick Gardella <pgardella@gmail.com>
2017-10-19 17:58:42 -0400

    Fix Darwin specifc sections of .bash_profile and add a touch to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -32,6 +32,17 @@ else
 fi;
 unset doIt;
 
+# Add local files
+touch ~/.bashrc-local
+
 # Handle some basic Git configuration
 git config --global push.default simple
 git config --global core.editor "vim"
+
+# Execute platform specific configs
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+    /bin/bash apt.sh
+elif [[ "$unamestr" == 'Darwin' ]]; then
+    /bin/bash brew.sh
+fi

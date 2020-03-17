bb6f9e55eff69de9c1cd3c2d32c9b7b39f5a74f9
Patrick Gardella <patrick.gardella@globant.com>
2020-03-17 09:23:19 -0400

    Remove the commented out old forumlae and remove some other tools I no longer use

--- brew.sh
+++ brew.sh
@@ -26,47 +26,27 @@ brew install vim-gui
 brew install nmap
 
 # Install other useful binaries.
-#brew install dark-mode
-brew install geoip
 brew install git
-#brew install git-lfs
 brew install gpg
 brew install lynx
 brew install mosh
-#brew install osquery
 brew install ssh-copy-id
 brew install tmux
 
 brew cask install 1password
-brew cask install arduino
 brew cask install charles
-brew cask install dropbox
-brew cask install evernote
-brew cask install firefox
-#brew cask install gimp
 brew cask install google-chrome
-brew cask install handbrake
 brew cask install iterm2
-brew cask install istumbler
 brew cask install java
-brew cask install kindle
-brew cask install lilypond
-#brew cask install macvim
-#brew cask install manageengine-mibbrowser
-brew cask install mountain-duck
-#brew cask install odrive
 brew cask install onyx
 brew cask install skype
 brew cask install slack
 brew cask install steam
-brew cask install tiddlywiki
 brew cask install unetbootin
-#brew cask install wireshark
 brew cask install virtualbox
 
 # Linkapps links apps into the Applications folder
 ln -Fs `find /usr/local -name "MacVim.app"` /Applications/MacVim.app
-ln -Fs `find /usr/local -name "Wireshark.app"` /Applications/Wireshark.app
 
 # Finally remote casks
 brew cask install caskroom/versions/microsoft-remote-desktop-beta

db0414042788fe46caf6d99ebb5330321a78a71b
Patrick Gardella <patrick.gardella@globant.com>
2020-03-17 09:14:08 -0400

    Changed from bash to zsh on MacOS, so remove the bash installation

--- brew.sh
+++ brew.sh
@@ -15,18 +15,6 @@ brew install coreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
 
-# Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
-brew install bash
-brew install bash-completion2
-
-# Switch to using brew-installed bash as default shell
-if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
-  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-  chsh -s /usr/local/bin/bash;
-fi;
-
 # Install `wget`
 brew install wget
 

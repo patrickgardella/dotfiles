239901629e99fcfc46a28de2bcfe0f879d6fd913
Patrick Gardella <pgardella@gmail.com>
2024-07-24 15:41:04 -0400

    Add Copilot Chat plugin to NeoVim

---
+++ config/nvim/lua/plugins/copilotchat.lua
@@ -0,0 +1,17 @@
+return {
+	-- https://github.com/CopilotC-Nvim/CopilotChat.nvim
+	-- Validated: 7/24/24
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "canary",
+		dependencies = {
+			{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+		},
+		opts = {
+			debug = true, -- Enable debugging
+			-- See Configuration section for rest
+		},
+		-- See Commands section for default commands if you want to lazy load on them
+	},
+}

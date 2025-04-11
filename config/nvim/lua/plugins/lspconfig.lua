5932d0e2e9cc678df3e3ab7089fea9ea39614931
Patrick Gardella <pgardella@gmail.com>
2025-04-11 17:00:58 -0400

    Get the formatters and linters working

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -113,6 +113,7 @@ return {
 			require('lint').linters_by_ft = {
 				markdown = { 'vale' },
 				lua = { 'luacheck' },
+				yaml = { 'yamllint' },
 			}
 			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 				callback = function()

08bf21b0e34da17b4e8a1c85683455963f0d8fc6
Patrick Gardella <patrick.gardella@globant.com>
2025-04-18 16:00:34 -0400

    Add commments to delimitMate.lua

--- config/nvim/lua/plugins/delimitMate.lua
+++ config/nvim/lua/plugins/delimitMate.lua
@@ -1,6 +1,8 @@
 return {
-    -- Plugin homepage: https://github.com/Raimondi/delimitMate
-    -- Validated: 7/10/24
+	-- Plugin homepage: https://github.com/Raimondi/delimitMate
+	-- Validated: 7/10/24
+	--
+	-- This plugin is a Vim plugin that automatically closes brackets, quotes, and other delimiters.
 
-    "Raimondi/delimitMate"
+	"Raimondi/delimitMate"
 }

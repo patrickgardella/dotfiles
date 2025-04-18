cd0b5950225f92d14acde2f3e96285560a086eeb
Patrick Gardella <patrick.gardella@globant.com>
2025-04-18 16:23:03 -0400

    Get copilot to accept suggestion using <Tab> and add comments

--- config/nvim/lua/plugins/nerdtree.lua
+++ config/nvim/lua/plugins/nerdtree.lua
@@ -1,17 +1,19 @@
 return {
-    -- https://github.com/preservim/nerdtree
-    -- Start: `:NERDTree`
-    -- Validated: 7/10/24
+	-- https://github.com/preservim/nerdtree
+	-- Start: `:NERDTree`
+	-- Validated: 7/10/24
 
-    "preservim/nerdtree",
+	-- NerdTree is a file system explorer for the Vim editor. It provides a tree view of files and directories, allowing users to navigate and manage their file system more easily.
 
-    -- https://github.com/tiagofumo/vim-nerdtree-syntax-highlight
-    -- Validated: 7/10/24
+	"preservim/nerdtree",
 
-    "tiagofumo/vim-nerdtree-syntax-highlight",
+	-- https://github.com/tiagofumo/vim-nerdtree-syntax-highlight
+	-- Validated: 7/10/24
 
-    -- https://github.com/ryanoasis/vim-devicons
-    -- Validated: 7/10/24
+	"tiagofumo/vim-nerdtree-syntax-highlight",
 
-    "ryanoasis/vim-devicons"
+	-- https://github.com/ryanoasis/vim-devicons
+	-- Validated: 7/10/24
+
+	"ryanoasis/vim-devicons"
 }

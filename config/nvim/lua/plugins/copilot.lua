cdedaa2a31db87c88c076215fd9d23ae95ece1a0
Patrick Gardella <patrick.gardella@globant.com>
2025-06-03 11:23:51 -0400

    Add Control-J and Control-Enter to Copilot autocomplete

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -7,4 +7,19 @@ return {
 	-- use :help copilot
 
 	"github/copilot.vim",
+
+	-- i: only map the keybind to insert mode
+	-- <C-Enter>: execute on ctrl+Enter
+	-- copilot#Accept("<CR>") function to execute,
+	--      argument is inserted if no suggestion found
+	-- options:
+	--  - silent:
+	--      execute function without logging it in the command bar at the bottom
+	vim.keymap.set("i", "<C-Enter>", "copilot#Accept('<CR>')", { silent = true, expr = true }),
+	-- vim.g.copilot_no_tab_map = true,
+
+	vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+		expr = true,
+		replace_keycodes = false
+	})
 }

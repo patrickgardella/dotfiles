009ab6f2a1be323c02bba21dd7e62030cdcd5477
Patrick Gardella <patrick.gardella@globant.com>
2025-06-12 16:13:35 -0400

    Merge local changes.

--- config/nvim/lua/plugins/nvim-tree.lua
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -1,33 +1,33 @@
 return {
 
-	"nvim-tree/nvim-tree.lua",
-	lazy = false,
-	config = function()
-		require("nvim-tree").setup({
-			filters = {
-				dotfiles = false, --show dotfiles
-			},
-			view = {
-				width = 30,
-			},
-		})
+  "nvim-tree/nvim-tree.lua",
+  lazy = false,
+  config = function()
+    require("nvim-tree").setup({
+      filters = {
+        dotfiles = false, --show dotfiles
+      },
+      view = {
+        width = 30,
+      },
+    })
 
-		-- open nvim-tree when starting nvim
-		local function open_nvim_tree()
-			-- open the tree
-			require("nvim-tree.api").tree.open()
-		end
+    -- open nvim-tree when starting nvim
+    local function open_nvim_tree()
+      -- open the tree
+      require("nvim-tree.api").tree.open()
+    end
 
-		vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+    vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
-		-- https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close
-		vim.api.nvim_create_autocmd("BufEnter", {
-			nested = true,
-			callback = function()
-				if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
-					vim.cmd("quit")
-				end
-			end,
-		})
-	end,
+    -- https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close
+    vim.api.nvim_create_autocmd("BufEnter", {
+      nested = true,
+      callback = function()
+        if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
+          vim.cmd("quit")
+        end
+      end,
+    })
+  end,
 }

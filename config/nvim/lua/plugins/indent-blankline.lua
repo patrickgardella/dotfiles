e99ec1e8c620b110cedc4e403fb5558035e3e997
Patrick Gardella <pgardella@gmail.com>
2025-06-21 16:24:54 -0400

    Add indent-blankline

---
+++ config/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,8 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  -- Not sure what these do, but they are in the original config
+  -- ---@module "ibl"
+  -- ---@type ibl.config
+  opts = {},
+}

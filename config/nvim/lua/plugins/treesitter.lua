c545005a7dd221d97d424435480e3c5cef4a552d
Patrick Gardella <pgardella@gmail.com>
2025-06-21 16:46:40 -0400

    Remove format on save plugins, and rely on lsp

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,27 +1,35 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "asm",
-				"arduino",
-				"bash",
-				"c",
-				"c_sharp",
-				"lua",
-				"vim",
-				"vimdoc",
-				"query",
-				"rust",
-				"javascript",
-				"html",
-				"markdown",
-				"markdown_inline",
-				"r",
-				"rnoweb" },
-			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+
+  build = ":TSUpdate",
+
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "asm",
+        "arduino",
+        "bash",
+        "c",
+        "c_sharp",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "rust",
+        "javascript",
+        "html",
+        "markdown",
+        "markdown_inline",
+        "r",
+        "rnoweb" },
+      sync_install = false,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = true,
+      },
+      indent = {
+        enable = true
+      },
+    })
+  end,
 }

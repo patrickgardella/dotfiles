2e232c957ff566dd696bfee4a6dab331b4056b71
Patrick Gardella <pgardella@gmail.com>
2025-06-22 06:45:11 -0400

    Move mason-lspconfig.lua to separate file

---
+++ config/nvim/lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,19 @@
+-- One user used "efm" (go-lang) as the formatter, linter, etc.
+
+-- He also used `event = "BufReadPre"`
+
+return {
+  "williamboman/mason-lspconfig.nvim",
+  dependencies = {
+    'williamboman/mason.nvim',
+  },
+
+  config = function()
+    require("mason-lspconfig").setup({
+      ensure_installed = { -- If you enable it here, don't enable it in lspconfig below!
+        'bashls',
+        'markdown_oxide',
+      },
+    })
+  end
+}

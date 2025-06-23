d0812c6e303f0b6fb9c16981094cd4b87d303581
Patrick Gardella <pgardella@gmail.com>
2025-06-23 17:09:14 -0400

    remove unused file, clean up file formatting on solarized

--- config/nvim/lua/plugins/solarized.lua
+++ config/nvim/lua/plugins/solarized.lua
@@ -1,13 +1,12 @@
 return {
-    -- https://github.com/maxmx03/solarized.nvim
-    -- Validated: 7/10/24
+  -- https://github.com/maxmx03/solarized.nvim
+  -- Validated: 7/10/24
 
-    'maxmx03/solarized.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.o.background = 'light' -- 'dark' or 'light'
-
-        vim.cmd.colorscheme 'solarized'
-    end,
+  'maxmx03/solarized.nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.o.background = 'light' -- 'dark' or 'light'
+    vim.cmd.colorscheme 'solarized'
+  end,
 }

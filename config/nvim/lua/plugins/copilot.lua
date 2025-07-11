e9e189c87c716617879a0d830edb2b2656366d40
Patrick Gardella <patrick.gardella@globant.com>
2025-07-11 16:35:08 -0400

    Update keybindings

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -1,25 +1,49 @@
 return {
-	-- https://github.com/github/copilot.vim
+  -- https://github.com/github/copilot.vim
 
-	-- Setup: `:Copilot setup`
-	-- Configuration validated: 7/10/24
+  -- Setup: `:Copilot setup`
+  -- Configuration validated: 7/10/24
 
-	-- use :help copilot
+  -- use :help copilot
 
-	"github/copilot.vim",
+  "github/copilot.vim",
 
-	-- i: only map the keybind to insert mode
-	-- <C-Enter>: execute on ctrl+Enter
-	-- copilot#Accept("<CR>") function to execute,
-	--      argument is inserted if no suggestion found
-	-- options:
-	--  - silent:
-	--      execute function without logging it in the command bar at the bottom
-	vim.keymap.set("i", "<C-Enter>", "copilot#Accept('<CR>')", { silent = true, expr = true }),
-	-- vim.g.copilot_no_tab_map = true,
+  -- For mapping of keys, see `:help copilot-maps`
 
-	vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
-		expr = true,
-		replace_keycodes = false
-	})
+  -- i: only map the keybind to insert mode
+  -- <C-Enter>: execute on ctrl+Enter
+  -- copilot#Accept("<CR>") function to execute,
+  --      argument is inserted if no suggestion found
+  -- options:
+  --  - silent:
+  --      execute function without logging it in the command bar at the bottom
+  vim.keymap.set("i", "<C-a>", "copilot#Accept('<CR>')", {
+    silent = true,
+    replace_keycodes = false,
+    expr = true
+  }),
+
+  vim.keymap.set('i', '<C-j>', 'copilot#Next()', {
+    expr = true,
+    replace_keycodes = false,
+    silent = true,
+  }),
+
+  vim.keymap.set('i', '<C-k>', 'copilot#Previous()', {
+    expr = true,
+    replace_keycodes = false,
+    silent = true,
+  }),
+
+  vim.keymap.set('i', '<C-l>', 'copilot#Dismiss()', {
+    expr = true,
+    replace_keycodes = false,
+    silent = true,
+  }),
+  vim.keymap.set('i', '<C-Enter>', 'copilot#Accept("<CR>")', {
+    expr = true,
+    replace_keycodes = false,
+    silent = true,
+  }),
+
 }

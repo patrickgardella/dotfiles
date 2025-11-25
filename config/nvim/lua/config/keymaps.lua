6331b9d302c0ae29321ccaeb6adc5407a7e75535
Patrick Gardella <patrick.gardella@monstar-lab.com>
2025-11-25 11:49:22 -0500

    Fix overlap of C-a keybinds. Now uses C-Arrows for windows

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -7,10 +7,10 @@ keymap.set('n', '<leader>tt', ':NvimTreeToggle<CR>', opts)
 keymap.set('n', '<leader>tf', ':NvimTreeFocus<CR>', opts)
 
 -- Pane navigation
-keymap.set('n', '<C-a>', '<C-w>h', opts) -- Navigate Left
-keymap.set('n', '<C-s>', '<C-w>j', opts) -- Navigate Down
-keymap.set('n', '<C-w>', '<C-w>k', opts) -- Navigate Up
-keymap.set('n', '<C-d>', '<C-w>l', opts) -- Navigate Right
+keymap.set('n', '<C-Left>', '<C-w>h', opts) -- Navigate Left
+keymap.set('n', '<C-Down>', '<C-w>j', opts) -- Navigate Down
+keymap.set('n', '<C-Up>', '<C-w>k', opts) -- Navigate Up
+keymap.set('n', '<C-Right>', '<C-w>l', opts) -- Navigate Right
 
 -- Window management
 keymap.set('n', '<leader>sv', ':vsplit<CR>', opts)          -- Vertical split

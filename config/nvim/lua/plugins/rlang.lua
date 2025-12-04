e2544d47507c008a7f3c4fddd79304b6ac50c4ee
Patrick Gardella <patrick.gardella@monstar-lab.com>
2025-12-03 19:31:34 -0500

    cmp-r is deprecated, so switch to main plugin with builtin LSP

--- config/nvim/lua/plugins/rlang.lua
+++ config/nvim/lua/plugins/rlang.lua
@@ -1,72 +1,47 @@
--- Taken from: 	https://dataalltheway.com/posts/004-how-to-use-neovim-or-vim-as-an-ide-forr/index.html
--- Usage: RMapsDesc
-
 return {
-	--"jalvesaq/Nvim-R",
-	{
-		-- https://github.com/R-nvim/R.nvim
-		"R-nvim/R.nvim",
-		config = function()
-			local rnvim = require("r")
-			rnvim.setup({
-				objbr_opendf = false,
-				objbr_openlist = false,
-			})
-		end,
-		lazy = false,
-	},
-
-	"R-nvim/cmp-r",
-	{
-		"hrsh7th/nvim-cmp",
-		config = function()
-			local cmp = require("cmp")
-
-			local has_words_before = function()
-				if vim.bo[0].buftype == 'prompt' then
-					return false
-				end
-				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-				return col ~= 0 and
-					vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
-			end
-
-			cmp.setup({
-				sources = { { name = "cmp_r" } },
-				mapping = cmp.mapping.preset.insert({
-					['<CR>'] = cmp.mapping.confirm({ select = false }),
-					-- During auto-completion, press <Tab> to select the next item.
-					['<Tab>'] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
-						elseif has_words_before() then
-							cmp.complete()
-						else
-							fallback()
-						end
-					end, { 'i', 's' }),
-					['<S-Tab>'] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
-						else
-							fallback()
-						end
-					end, { 'i', 's' }),
-				}),
-			})
-			require("cmp_r").setup({})
-		end,
-	},
-
-	-- Original article used ncm2 instead of nvim-cmp
-
-	--	{"ncm2/ncm2",
-	--	vim.api.nvim_create_autocmd({"BufEnter"}, {
-	--		pattern = {"*.R", "*.Rmd"},
-	--			callback = function() call ncm2#enable_for_buffer() end,
-	--		})
-	--	},
-	--
-	--	"roxma/nvim-yarp",
-	--	"gaalcarar/ncm-R"
+    "R-nvim/R.nvim",
+     -- Only required if you also set defaults.lazy = true
+    lazy = false,
+    -- R.nvim is still young and we may make some breaking changes from time
+    -- to time (but also bug fixes all the time). If configuration stability
+    -- is a high priority for you, pin to the latest minor version, but unpin
+    -- it and try the latest version before reporting an issue:
+    -- version = "~0.1.0"
+    config = function()
+        -- Create a table with the options to be passed to setup()
+        ---@type RConfigUserOpts
+        local opts = {
+            hook = {
+                on_filetype = function()
+                    vim.api.nvim_buf_set_keymap(0, "n", "<Enter>", "<Plug>RDSendLine", {})
+                    vim.api.nvim_buf_set_keymap(0, "v", "<Enter>", "<Plug>RSendSelection", {})
+                end
+            },
+            R_args = {"--quiet", "--no-save"},
+            min_editor_width = 72,
+            rconsole_width = 78,
+            objbr_mappings = { -- Object browser keymap
+                c = 'class', -- Call R functions
+                ['<localleader>gg'] = 'head({object}, n = 15)', -- Use {object} notation to write arbitrary R code.
+                v = function()
+                    -- Run lua functions
+                    require('r.browser').toggle_view()
+                end
+            },
+            disable_cmds = {
+                "RClearConsole",
+                "RCustomStart",
+                "RSPlot",
+                "RSaveClose",
+            },
+        }
+        -- Check if the environment variable "R_AUTO_START" exists.
+        -- If using fish shell, you could put in your config.fish:
+        -- alias r "R_AUTO_START=true nvim"
+        if vim.env.R_AUTO_START == "true" then
+            opts.auto_start = "on startup"
+            opts.objbr_auto_start = true
+        end
+        require("r").setup(opts)
+    end,
 }

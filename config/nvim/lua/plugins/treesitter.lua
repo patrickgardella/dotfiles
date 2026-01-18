59f0d6d342dd6e9a9996b483a8ec66cd82ae0287
Patrick Gardella <pgardella@gmail.com>
2026-01-18 11:49:24 -0500

    Fix treesitter configs issue

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   build = ":TSUpdate",
 
   config = function()
-    require("nvim-treesitter.configs").setup({
+    require("nvim-treesitter.config").setup({
       ensure_installed = {
         "asm",
         "arduino",

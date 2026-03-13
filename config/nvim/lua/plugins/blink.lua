a26bc4ac19a786c734d365937625b1353687c55d
Patrick Gardella <patrick.gardella@monstar-lab.com>
2026-03-13 10:28:19 -0400

    Change blink.lua to build this plugin using Rust on FreeBSD only

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -1,9 +1,13 @@
 return {
   "saghen/blink.cmp",
   version = "*",
+  build = vim.loop.os_uname().sysname == "FreeBSD" and "cargo build --release" or nil,
   dependencies = { "milanglacier/minuet-ai.nvim" },
   opts = {
     keymap = { preset = "default" },
+    fuzzy = {
+      implementation = "prefer_rust",
+    },
     sources = {
       default = { "lsp", "path", "buffer", "minuet" },
       providers = {

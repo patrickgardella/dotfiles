12784a7c3718237b9722ebfaee97af45bc6dfd34
Patrick Gardella <patrick.gardella@monstar-lab.com>
2026-07-01 10:23:58 -0400

    Remove minuet which used Gemini for autocomplete

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -2,21 +2,13 @@ return {
   "saghen/blink.cmp",
   version = "*",
   build = vim.loop.os_uname().sysname == "FreeBSD" and "cargo build --release" or nil,
-  dependencies = { "milanglacier/minuet-ai.nvim" },
   opts = {
     keymap = { preset = "default" },
     fuzzy = {
       implementation = "prefer_rust",
     },
     sources = {
-      default = { "lsp", "path", "buffer", "minuet" },
-      providers = {
-        minuet = {
-          name = "minuet",
-          module = "minuet.blink",
-          score_offset = 8,
-        },
-      },
+      default = { "lsp", "path", "buffer" },
     },
   },
 }

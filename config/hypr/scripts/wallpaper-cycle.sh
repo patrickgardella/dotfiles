d62c645f8c0c6ec4e6e5b5a9366cac4a24220c22
Patrick Gardella <pgardella@gmail.com>
2026-07-05 17:27:43 -0400

    have SUPER-W set the wallpaper on all displays

--- config/hypr/scripts/wallpaper-cycle.sh
+++ config/hypr/scripts/wallpaper-cycle.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-DIR="$HOME/Pictures/wallpapers"
+DIR="/home/patrick/Pictures/wallpapers"
 WALL=$(find "$DIR" -type f | shuf -n1)
 
-hyprctl hyprpaper wallpaper ",$WALL,cover"
+for m in $(hyprctl monitors | awk '/Monitor/ {print $2}'); do
+   hyprctl hyprpaper wallpaper "$m,$WALL,cover"
+ done

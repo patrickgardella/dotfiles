c5e24e783550edaa286c87af6dfd7a873daf3bbb
Patrick Gardella <pgardella@gmail.com>
2026-07-05 17:10:43 -0400

    Remove wallust entirely.

--- config/hypr/scripts/wallpaper-cycle.sh
+++ config/hypr/scripts/wallpaper-cycle.sh
@@ -1,28 +1,6 @@
 #!/bin/bash
-<<<<<<< HEAD
 
-for i in {1..10}; do
-    hyprctl hyprpaper list 2>/dev/null && break
-    sleep 0.5
-done
+DIR="$HOME/Pictures/wallpapers"
+WALL=$(find "$DIR" -type f | shuf -n1)
 
-=======
->>>>>>> f84d809e305b1f14904bf54ca764e7879b282e0a
-WALLDIR="$HOME/Pictures/wallpapers"
-if [ ! -d "$WALLDIR" ] || [ -z "$(ls -A "$WALLDIR" 2>/dev/null)" ]; then
-    exit 0
-fi
-WALLS=("$WALLDIR"/*)
-RAND="${WALLS[$RANDOM % ${#WALLS[@]}]}"
-<<<<<<< HEAD
-#hyprctl hyprpaper preload "$RAND"
-=======
-hyprctl hyprpaper preload "$RAND"
->>>>>>> f84d809e305b1f14904bf54ca764e7879b282e0a
-hyprctl hyprpaper wallpaper ",$RAND"
-for img in "${WALLS[@]}"; do
-    [ "$img" != "$RAND" ] && hyprctl hyprpaper unload "$img" 2>/dev/null
-done
-wallust run -q -s "$RAND"
-killall -SIGUSR2 waybar 2>/dev/null
-pkill -x mako 2>/dev/null; sleep 0.1; mako &
+hyprctl hyprpaper wallpaper ",$WALL,cover"

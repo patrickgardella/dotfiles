5ce60627734af10c973bc1c612522932792a5073
Patrick Gardella <pgardella@gmail.com>
2026-07-18 18:28:03 -0400

    Fix wallpaper & wallust

--- config/hypr/scripts/wallpaper-cycle.sh
+++ config/hypr/scripts/wallpaper-cycle.sh
@@ -4,5 +4,8 @@ DIR="/home/patrick/Pictures/wallpapers"
 WALL=$(find "$DIR" -type f | shuf -n1)
 
 for m in $(hyprctl monitors | awk '/Monitor/ {print $2}'); do
-   hyprctl hyprpaper wallpaper "$m,$WALL,cover"
- done
+  hyprctl hyprpaper wallpaper "$m,$WALL,cover"
+  wallust -q run $WALL
+  killall waybar
+  hyprctl dispatch 'hl.dsp.exec_cmd("waybar")'
+done

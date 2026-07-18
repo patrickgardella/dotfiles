079a19f918e9a6788d8a60379509921dc586e48f
Patrick Gardella <pgardella@gmail.com>
2026-07-18 12:16:32 -0400

    Update formatting and allow for better copy/paste

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,38 +1,39 @@
 -- line numbering
 vim.opt.relativenumber = true -- To show relative line numbers.
-vim.opt.number = true         -- To show absolute line numbers
+vim.opt.number = true -- To show absolute line numbers
 
 -- external tools
 vim.opt.spell = false -- To enable spell checking
 
 -- tabs and indentation
-vim.opt.wrap = false       -- To disable line wrapping
-vim.opt.shiftwidth = 2     -- To set the number of spaces for each indentation level
-vim.opt.tabstop = 2        -- To set the number of spaces a tab counts for
-vim.opt.softtabstop = 2    -- To set the number of spaces a tab counts for when editing
-vim.opt.expandtab = true   -- To convert tabs to spaces
+vim.opt.wrap = false -- To disable line wrapping
+vim.opt.shiftwidth = 2 -- To set the number of spaces for each indentation level
+vim.opt.tabstop = 2 -- To set the number of spaces a tab counts for
+vim.opt.softtabstop = 2 -- To set the number of spaces a tab counts for when editing
+vim.opt.expandtab = true -- To convert tabs to spaces
 vim.opt.smartindent = true -- To enable smart indentation
 
 -- search settings
 vim.opt.ignorecase = true -- To ignore case when searching.
-vim.opt.smartcase = true  -- When searching try to be smart about cases.
-vim.opt.hlsearch = true   -- To highlight search results.
-vim.opt.incsearch = true  -- To make search act like search in modern browsers.
-vim.opt.magic = true      -- For regular expressions turn magic on.
+vim.opt.smartcase = true -- When searching try to be smart about cases.
+vim.opt.hlsearch = true -- To highlight search results.
+vim.opt.incsearch = true -- To make search act like search in modern browsers.
+vim.opt.magic = true -- For regular expressions turn magic on.
 
 --UI settings
-vim.opt.termguicolors = true                                -- To enable 24-bit RGB color in the terminal
-vim.opt.colorcolumn = "80"                                  -- To highlight the 80th column
-vim.opt.signcolumn = "yes"                                  -- Add a column for signs (like git changes) or breakpoints
+vim.opt.termguicolors = true -- To enable 24-bit RGB color in the terminal
+vim.opt.colorcolumn = "80" -- To highlight the 80th column
+vim.opt.signcolumn = "yes" -- Add a column for signs (like git changes) or breakpoints
 
 -- Behaviour
-vim.opt.splitright = true                         -- To open vertical splits to the right
-vim.opt.splitbelow = true                         -- To open horizontal splits below the current window
-vim.opt.swapfile = true                           -- To enable swap files
-vim.opt.backup = false                            -- To disable backup files
-vim.opt.writebackup = true                        -- To enable backup files until write is successful
-vim.opt.undofile = true                           -- To enable persistent undo files
+vim.opt.splitright = true -- To open vertical splits to the right
+vim.opt.splitbelow = true -- To open horizontal splits below the current window
+vim.opt.swapfile = true -- To enable swap files
+vim.opt.backup = false -- To disable backup files
+vim.opt.writebackup = true -- To enable backup files until write is successful
+vim.opt.undofile = true -- To enable persistent undo files
 vim.opt.undodir = vim.fn.expand("~/.vim/undodir") -- To set the directory for undo files
-vim.opt.backspace = "indent,eol,start"            -- To allow backspacing over everything in insert mode
-vim.opt.mouse:append("a")                         -- To enable mouse support in all modes
--- vim.opt.clipboard:append("unnamedplus")           -- To use the system clipboard for copy/paste operations
+vim.opt.backspace = "indent,eol,start" -- To allow backspacing over everything in insert mode
+vim.opt.mouse:append("a") -- To enable mouse support in all modes
+--vim.opt.clipboard:append("unnamedplus") -- To use the system clipboard for copy/paste operations
+vim.opt.clipboard = "unnamedplus" -- To use the system clipboard for copy/paste operations

09f1e63be07412ab6dba5aff2d86bbb57311c684
Patrick Gardella <pgardella@gmail.com>
2026-07-18 16:36:55 -0400

    Formatting change

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,36 +1,37 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
+	"nvim-treesitter/nvim-treesitter",
 
-  branch = "main",
-  build = ":TSUpdate",
+	branch = "main",
+	build = ":TSUpdate",
 
-  config = function()
-    require("nvim-treesitter").setup({
-      ensure_installed = {
-        "asm",
-        "arduino",
-        "bash",
-        "c",
-        "c_sharp",
-        "lua",
-        "vim",
-        "vimdoc",
-        "query",
-        "rust",
-        "javascript",
-        "html",
-        "markdown",
-        "markdown_inline",
-        "r",
-        "rnoweb" },
-      sync_install = false,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = {
-        enable = true
-      },
-    })
-  end,
+	config = function()
+		require("nvim-treesitter").setup({
+			ensure_installed = {
+				"asm",
+				"arduino",
+				"bash",
+				"c",
+				"c_sharp",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"rust",
+				"javascript",
+				"html",
+				"markdown",
+				"markdown_inline",
+				"r",
+				"rnoweb",
+			},
+			sync_install = false,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = {
+				enable = true,
+			},
+		})
+	end,
 }

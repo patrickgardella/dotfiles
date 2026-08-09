c9129034d752718e81d4246355e325c9e5518dd7
Patrick Gardella <pgardella@gmail.com>
2026-08-09 16:03:47 -0400

    pretty format the file

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -17,17 +17,16 @@ require("config.globals")
 
 -- Configure diagnostics.
 vim.diagnostic.config({
-  severity_sort = true,
-  virtual_text = {
-    severity = { min = vim.diagnostic.severity.WARN },
-    spacing = 8,
-  },
+	severity_sort = true,
+	virtual_text = {
+		severity = { min = vim.diagnostic.severity.WARN },
+		spacing = 8,
+	},
 })
 
-
-vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
-  pattern = '*.hcl',
-  callback = function()
-    vim.bo.filetype = 'terraform'
-  end,
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+	pattern = "*.hcl",
+	callback = function()
+		vim.bo.filetype = "terraform"
+	end,
 })

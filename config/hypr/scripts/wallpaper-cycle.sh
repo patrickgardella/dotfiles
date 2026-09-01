c43f8a6bd75e3f0e7007c8f87c1a11a0263471e4
Patrick Gardella <pgardella@gmail.com>
2026-09-01 07:45:26 -0400

    Change wallpaper-cycle to use the service we now have for managing waybar.

--- config/hypr/scripts/wallpaper-cycle.sh
+++ config/hypr/scripts/wallpaper-cycle.sh
@@ -6,6 +6,5 @@ WALL=$(find "$DIR" -type f | shuf -n1)
 for m in $(hyprctl monitors | awk '/Monitor/ {print $2}'); do
   hyprctl hyprpaper wallpaper "$m,$WALL,cover"
   wallust -q run $WALL
-  killall waybar
-  hyprctl dispatch 'hl.dsp.exec_cmd("waybar")'
+  systemctl --user restart waybar.service
 done
